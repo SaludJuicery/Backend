2c48aa331ebbe3d344f68a3dded48d78f6f022f9
EC2 Default User <ec2-user@ip-172-31-30-194.us-west-2.compute.internal>
2015-10-05 02:45:21 +0000

    API /menu/createCategory working with Bookshelf.js

--- routes/menu/createCategory.js
+++ routes/menu/createCategory.js
@@ -1,18 +1,47 @@
 // custom library
 // model
 var categoryTable = require('../../models/categoryTable').menu_categories;
-var mysql      = require('mysql'); // trying this instead of bookshelf
+/*var mysql      = require('mysql'); // trying this instead of bookshelf
 //connect to the database
     var pool = mysql.createPool({
 		host: 'saluddb.cazv88oyo3vo.us-west-2.rds.amazonaws.com',  // your host
-		user: 'salud',
-		password: 'SaludRDS', 
-		database: 'saluddb',
-		port: 3306
-	});
+		user: 'process.env.salud_db_username',
+		password: 'process.env.salud_db_password', 
+		database: 'process.env.salud_db_database',
+		port: process.env.salud_db_port
+	});*/
 
-//our callback goes here
+
 exports.insertCategory = function (req, res, next) {
+	//Checking first, if the user is already authenticated => his session is alive! => there exists a 'user' object set in the req
+    if (!req.user) 
+        return res.send({ "Message": "101" }); // User Not Authenticated
+
+    //checking if user has access to the resource
+    if (req.user.username !== 'salud.partner@gmail.com')
+    	return res.send({ "Message": "103" }); // User do not have access to this resource
+    
+    //checking if required data is present
+    if(!req.body.category_name)
+    	return res.send({ "Message": "401" }); // Data not found in post request
+
+    console.log("Pushing category name : "+req.body.category_name+" to menu_categories table");
+
+    //using the mapped tabed(bookshelf.js maps it for us)
+    categoryTable.forge({category_name: req.body.category_name}).save().then(function (rows){
+    	console.log(rows);
+    	return res.send({ "Message": "777" }); // Operation Success
+    }).catch(function (err){
+    	console.log(err)
+    	return res.send({ "Message": "403" }); // Database error
+    });
+};
+
+
+
+//Below method is written using MySQL library....Bookshelf gives more readability to code
+
+/*exports.insertCategoryMySQL = function (req, res, next) {
     //Checking first, if the user is already authenticated => his session is alive! => there exists a 'user' object set in the req
     if (!req.user) 
         return res.send({ "Message": "101" }); // User Not Authenticated
@@ -56,3 +85,4 @@ exports.insertCategory = function (req, res, next) {
     	});
 	});
 };
+*/
